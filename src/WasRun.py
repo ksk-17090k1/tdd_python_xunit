46dc5f9d45ab6a7cc3bf9c2276589f7cbb0d53d9
inagaki-keisuke <kin71796@gmail.com>
2024-06-26 22:12:19 +0900

    created "run" func

--- src/WasRun.py
+++ src/WasRun.py
@@ -4,5 +4,8 @@ from pydantic import BaseModel
 class WasRun(BaseModel):
     wasRun: str = None
 
+    def run(self):
+        self.testMethod()
+
     def testMethod(self):
         self.wasRun = 1
--- src/xunit.py
+++ src/xunit.py
@@ -2,5 +2,5 @@ from WasRun import WasRun
 
 test = WasRun(wasRun="testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
