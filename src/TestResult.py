338114f5ad482bc93e218e903c6c334e232f9bfe
inagaki-keisuke <kin71796@gmail.com>
2024-06-29 01:06:16 +0900

    product impl

--- src/TestResult.py
+++ src/TestResult.py
@@ -2,5 +2,13 @@ from pydantic import BaseModel
 
 
 class TestResult(BaseModel):
+    run_count: int = 0
+    failed_count: int = 0
+
+    def testStarted(self):
+        self.run_count += 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        run_count = 1
+        failed_count = 0
+        return f"{run_count} run, {failed_count} failed"
